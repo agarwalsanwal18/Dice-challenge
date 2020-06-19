b41fbee044a13a69659783a29a008de8cef87ef2
agarwalsanwal18 <62502240+agarwalsanwal18@users.noreply.github.com>
2020-06-19 18:58:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(6 * (Math.random())) + 1;
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomNumber2 = Math.floor(6 * (Math.random())) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImage1);
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
